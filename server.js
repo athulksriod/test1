fda0f613e9574e0328a1e802e0e7bfb47672ade4
athulksriod <145340016+athulksriod@users.noreply.github.com>
2024-03-15 12:42:11 +0530

    server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.post('/', (req, res) => {
 // Handle GET request for retrieving the last posted data
 app.get('/lastPostedData', (req, res) => {
     console.log(lastPostedData);
-    res.json({"test:",lastPostedData});
+    res.json(lastPostedData);
 });
 
 // Start the server
